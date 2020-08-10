71f4577ef10a011a6534f0b4db9abad5d0455403
minhee <xwoud@naver.com>
2020-08-10 13:21:29 +0900

    프로그래머스 - 카펫

---
+++ programmers/카펫.py
@@ -0,0 +1,16 @@
+def solution(brown, yellow):
+    answer = []
+    list = []
+    for i in range(yellow,0,-1):
+        if yellow//i > i :
+            break
+        if yellow % i == 0 :
+            list.append([i,yellow//i])
+
+    while list :
+        min = list.pop()
+        if (min[0]+2) * (min[1]+2) == brown+yellow :
+            answer = [min[0]+2,min[1]+2]
+            break
+            
+    return answer
