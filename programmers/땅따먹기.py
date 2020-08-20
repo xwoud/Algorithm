7b50f66b641f722cb0a8183af0663c5bfc3332c1
minhee <xwoud@naver.com>
2020-08-20 22:05:31 +0900

    프로그래머스 - 땅따먹기 제출

---
+++ programmers/땅따먹기.py
@@ -0,0 +1,14 @@
+
+
+def solution(land):
+
+    # n행 4열
+    for i in range(len(land) - 1):
+        land[i + 1][0] = max(land[i][1], land[i][2], land[i][3]) + land[i+1][0]
+        land[i + 1][1] = max(land[i][0], land[i][2], land[i][3]) + land[i+1][1]
+        land[i + 1][2] = max(land[i][0], land[i][1], land[i][3]) + land[i+1][2]
+        land[i + 1][3] = max(land[i][0], land[i][1], land[i][2]) + land[i+1][3]
+
+    return max(land[-1])
+
+print(solution([[1,2,3,5],[5,6,7,8],[4,3,2,1]]))
