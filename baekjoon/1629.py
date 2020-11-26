a8114b5d6b624c8dfe6d5c825bd9688d0d1fc905
xwoud <xwoud@naver.com>
2020-11-26 16:07:32 +0900

    백준 1629 - 곱셉 제출

---
+++ baekjoon/1629.py
@@ -0,0 +1,16 @@
+from __future__ import print_function
+import sys
+
+def power(a, b):
+    if b == 1:
+        return a % c
+    else:
+        temp = power(a, b // 2)
+        if b % 2 == 0:
+            return temp * temp % c
+        else:
+            return temp * temp * a % c
+
+
+a,b,c = map(int, sys.stdin.readline().split())
+print(power(a,b))
