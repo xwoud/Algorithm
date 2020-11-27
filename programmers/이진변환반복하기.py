92487de9b234e4dd75593d3eb94f6274c5d1a36b
xwoud <xwoud@naver.com>
2020-11-27 15:00:31 +0900

    프로그래머스 - 이진 변환 반복하기 제출

---
+++ programmers/이진변환반복하기.py
@@ -0,0 +1,9 @@
+def solution(s):
+    answer = [0,0]
+    while(s!="1"):
+        answer[1] += s.count("0")
+        s = format(len(s)-s.count("0"), 'b')
+        answer[0] += 1
+    return answer
+
+print(solution("1111111"))
