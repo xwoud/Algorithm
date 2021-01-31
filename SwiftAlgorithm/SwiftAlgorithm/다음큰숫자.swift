4eb05856f0e795d8630d5121e6111d67b7886c79
xwoud <xwoud@naver.com>
2021-01-31 23:54:49 +0900

    프로그래머스 - 다음 큰 숫자 풀이

--- SwiftAlgorithm/SwiftAlgorithm/다음큰숫자.swift
+++ SwiftAlgorithm/SwiftAlgorithm/다음큰숫자.swift
@@ -7,6 +7,21 @@
 
 import Foundation
 
+/// [다음 큰 숫자](https://programmers.co.kr/learn/courses/30/lessons/12911)
+/// - Parameter n: 기준 숫자
+/// - Returns: 2진수로 변환 시 n과 1의 갯수가 같은 n 다음으로 큰 수
+
+/**
+ 
+ 먼저 기준이 되는 n의 2진수로 변환 시 1의 갯수를 세어서 저장해준다.
+ 
+ 그리고 while문은 처음에는 그냥 true문으로 돌고 if 문으로 검사 후에 break문을 걸려고 했으나,
+ 
+ 그냥 while문 자체에서 검사를 해주는 것이 낫다고 판단해서 while문에서 1의 갯수가 같은지 판단했다.
+ 
+ number의 숫자를 계속 +1 해주면서 2진수로 바꿔가며 1의 갯수를 파악했다.
+ 
+ */
 func solution(_ n:Int) -> Int
 {
     var answer: Int = 0
