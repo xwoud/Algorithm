7b467fa73e659d1121c29f4c6874d403b7e4a487
xwoud <xwoud@naver.com>
2021-02-07 18:57:57 +0900

    프로그래머스 - 메뉴리뉴얼 제출 파이썬

--- programmers/메뉴리뉴얼.py
+++ programmers/메뉴리뉴얼.py
@@ -1,14 +1,14 @@
 from itertools import combinations
+from collections import Counter
 
 def solution(orders, course):
     answer = []
-    numbers = [0]*10001
-    print(numbers)
-    for k in range(0,len(orders),1):
-        for i in range(2, len(orders[k])):
-            if answer not in combinations(orders[k], i):
-                print(combinations(orders[k], i))
-            # print(list(map(''.join, combinations(orders[k], i))))
+    for i in course:
+        number = []
+        for j in orders:
+            number += list(map(''.join, combinations(sorted(j), i)))
+        for a,b in Counter(number).most_common():
+            if b == max(Counter(number).values()) and max(Counter(number).values()) > 1:
+                answer.append(a)
+    answer.sort()
     return answer
-
-print(solution(["ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"],[2,3,4]))
