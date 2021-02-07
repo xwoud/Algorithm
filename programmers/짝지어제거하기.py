670c37b9a3973d556199572d2c33e03cff1ee3b6
xwoud <xwoud@naver.com>
2021-02-07 19:45:53 +0900

    프로그래머스 - 짝 지어 제거하기 파이썬 제출

---
+++ programmers/짝지어제거하기.py
@@ -0,0 +1,15 @@
+def solution(s):
+    stacks = []
+    for i in s:
+        if len(stacks) > 0:
+            if stacks[-1] == i:
+                stacks.pop()
+            else:
+                stacks.append(i)
+        else:
+            stacks.append(i)
+
+    if stacks :
+        return 0
+    else :
+        return 1
