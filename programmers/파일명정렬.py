4f37384e847a09832ee0317ae6c6b05ed2dc30c9
xwoud <xwoud@naver.com>
2021-02-14 17:09:35 +0900

    프로그래머스 - 파일명 정렬 제출 파이썬

---
+++ programmers/파일명정렬.py
@@ -0,0 +1,15 @@
+import re
+
+def solution(files):
+    lists = []
+    answer = []
+    
+    for i in files:
+        lists.append(re.split("([0-9]+)",i))
+
+    my = sorted(lists, key = lambda x: (x[0].lower(), int(x[1])))
+
+    for j in my :
+        answer.append("".join(j))
+
+    return answer
