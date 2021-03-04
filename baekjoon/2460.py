5cd37b32831b4eb3b9770e186c92bde626e53843
xwoud <xwoud@naver.com>
2021-03-04 17:26:40 +0900

    백준 2460 지능형기차2 파이썬 제출

---
+++ baekjoon/2460.py
@@ -0,0 +1,22 @@
+import sys이
+
+# 비교를 이용한 풀
+answer = 0
+maxPeople = 0
+for _ in range(10):
+    n, m = map(int, sys.stdin.readline().split())
+    answer += m - n
+    if answer > maxPeople :
+        maxPeople = answer
+
+print(maxPeople)
+
+# 배열을 이용한 풀이
+# answer = 0
+# answerList = []
+# for _ in range(10):
+#     n, m = map(int, sys.stdin.readline().split())
+#     answer += (m - n)
+#     answerList.append(answer)
+#
+# print(max(answerList))
