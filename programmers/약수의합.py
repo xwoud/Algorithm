0c117a70597dd1c69d05cb0a3e1864b91829d4fc
xwoud <xwoud@naver.com>
2021-03-28 14:02:25 +0900

    프로그래머스 - 약수의 합 제출

---
+++ programmers/약수의합.py
@@ -0,0 +1,14 @@
+def solution(n):
+    divisorsList = []
+
+    for i in range(1, int(n ** (1 / 2)) + 1):
+        if (n % i == 0):
+            divisorsList.append(i)
+            if ((i ** 2) != n):
+                divisorsList.append(n // i)
+
+    divisorsList.sort()
+
+    return sum(divisorsList)
+
+print(solution(12))
