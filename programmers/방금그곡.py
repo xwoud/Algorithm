d829901c5356697bdee8e1461bbe898eb5c24e4a
xwoud <xwoud@naver.com>
2021-03-28 13:47:57 +0900

    프로그래머스 - 방금 그 곡 파이썬 제출

---
+++ programmers/방금그곡.py
@@ -0,0 +1,32 @@
+from datetime import datetime
+
+def replaceFunc(s):
+    s = s.replace('A#', 'M')
+    s = s.replace('F#', 'I')
+    s = s.replace('C#', 'N')
+    s = s.replace('D#', 'H')
+    s = s.replace('G#', 'I')
+    return s
+
+def solution(m, musicinfos):
+    m = replaceFunc(m)
+    answer = []
+    for i in musicinfos:
+        start, end, title, melody = i.split(',')
+        melody = replaceFunc(melody)
+
+        time = str(datetime.strptime(end, '%H:%M') - datetime.strptime(start,'%H:%M')).split(':')
+        total = int(time[0])*60 + int(time[1]) # 분으로 환산
+
+        totlaMelody = melody * (total // len(melody)) + melody[:(total%len(melody))]
+
+        if m in totlaMelody:
+            answer.append([title,total])
+
+    if len(answer) == 1 :
+        return answer[0][0]
+    elif len(answer) == 0 :
+        return "(None)"
+    else :
+        answer = sorted(answer, key=(lambda x: int(-x[1])))
+        return answer[0][0]
