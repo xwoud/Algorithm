da3bbbf9ddad3cdcc4d7fff289b188cc82497342
xwoud <xwoud@naver.com>
2021-04-25 12:42:03 +0900

    백준 1062 - 가르침 파이썬 제출

--- baekjoon/1062.py
+++ baekjoon/1062.py
@@ -1,39 +1,38 @@
-import sys
-from collections import deque
-input = sys.stdin.readline
+from sys import stdin, exit
 
-n, m = map(int, input().split())
-arr = []
+def dfs(idx, cnt):
+    global answer
 
-shark = deque()
-for i in range(n):
-    temp = list(map(int, input().split()))
-    for t in range(m):
-        if temp[t] == 1:
-            shark.append((i,t))
-    arr.append(temp)
+    if cnt == k - 5:
+        read_cnt = 0
+        for word in words:
+            for w in word:
+                if not learn[ord(w) - ord('a')]:
+                    break
+            else:
+                read_cnt += 1
+        answer = max(answer, read_cnt) if answer else read_cnt
+        return
 
-mx = [-1, -1, -1, 0, 1, 0, 1, 1]
-my = [-1, 0, 1, 1, 1, -1, 0, -1]
-
-
-def bfs():
-    while shark:
-        x, y = shark.popleft()
-        for k in range(8):
-            dx = x + mx[k]
-            dy = y + my[k]
-            if 0 <= dx < n and 0 <= dy < m:
-                if arr[dx][dy] == 0:
-                    shark.append((dx,dy))
-                    arr[dx][dy] = arr[x][y] + 1
-    return
+    for i in range(idx, 26):
+        if not learn[i]:
+            learn[i] = True
+            dfs(i, cnt + 1)
+            learn[i] = False
 
 
-bfs()
-safe_dist = 0
-for i in range(n):
-    for j in range(m):
-        safe_dist = max(safe_dist, arr[i][j])
+answer = 0
+n, k = map(int, stdin.readline().split())
 
-print(safe_dist - 1)
+if k < 5 or k == 26:
+    print(0 if k < 5 else n)
+    exit(0)
+
+words = [set(stdin.readline().rstrip()) for _ in range(n)]
+learn = [False] * 26
+
+for c in ('a', 'c', 'i', 'n', 't'):
+    learn[ord(c) - ord('a')] = True
+
+dfs(0, 0)
+print(answer)
