6aa1e947202c547a525640a9a3a37868b6f185a3
xwoud <xwoud@naver.com>
2021-05-09 16:46:02 +0900

    프로그래머스 - 행렬 테두리 회전하기 파이썬 제출

---
+++ programmers/행렬테두리회전하기.py
@@ -0,0 +1,37 @@
+def solution(rows, columns, queries):
+    answer = []
+    array = [[0 for col in range(columns)] for row in range(rows)]
+    t = 1
+    for row in range(rows):
+        for col in range(columns):
+            array[row][col] = t
+            t += 1
+
+    for x1,y1,x2,y2 in queries:
+        tmp = array[x1-1][y1-1]
+        mini = tmp
+
+        for k in range(x1-1,x2-1):
+            test = array[k+1][y1-1]
+            array[k][y1-1] = test
+            mini = min(mini, test)
+
+        for k in range(y1-1,y2-1):
+            test = array[x2-1][k+1]
+            array[x2-1][k] = test
+            mini = min(mini, test)
+
+        for k in range(x2-1,x1-1,-1):
+            test = array[k-1][y2-1]
+            array[k][y2-1] = test
+            mini = min(mini, test)
+
+        for k in range(y2-1,y1-1,-1):
+            test = array[x1-1][k-1]
+            array[x1-1][k] = test
+            mini = min(mini, test)
+
+        array[x1-1][y1] = tmp
+        answer.append(mini)
+
+    return answer
