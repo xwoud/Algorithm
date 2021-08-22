becdd213bf94a33f44225c60ecb48f054245dbe0
xwoud <xwoud@naver.com>
2021-08-22 16:07:22 +0900

    [uplolad] LeetCode 228. Summary Ranges 파이썬 제출

---
+++ LeetCode/228.py
@@ -0,0 +1,18 @@
+class Solution:
+    def summaryRanges(self, nums: list[int]) -> list[str]:
+        answer = []
+        start = 0
+        end = 0
+
+        while start < len(nums):
+            while end + 1 < len(nums) and nums[end + 1] == nums[end] + 1:
+                end += 1
+            answer.append(f"{nums[start]}->{nums[end]}" if start != end else f"{nums[start]}")
+            start = end + 1
+            end = start
+
+        return answer
+
+
+my = Solution()
+print(my.summaryRanges([-4,-3,-2]))
