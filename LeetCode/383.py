71b001cd353e27fcaf9dbff74748ee1930f21051
xwoud <xwoud@naver.com>
2021-09-19 23:19:50 +0900

    [upload] LeetCode 383. Ransom Note 파이썬 제출

---
+++ LeetCode/383.py
@@ -0,0 +1,8 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        for i in ransomNote:
+            if i in magazine:
+                magazine = magazine.replace(i, '', 1)
+            else:
+                return False
+        return True
