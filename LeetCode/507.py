940c2953d33f5d39f1a6cf44ae60ca7f73e434f6
xwoud <xwoud@naver.com>
2021-10-03 01:50:53 +0900

    [upload] LeetCode 507. Perfect Number 파이썬 제출

---
+++ LeetCode/507.py
@@ -0,0 +1,20 @@
+class Solution:
+    def getMyDivisor(self, n: int):
+
+        divisorsList = []
+
+        for i in range(1, int(n ** (1 / 2)) + 1):
+            if (n % i == 0):
+                divisorsList.append(i)
+                if ((i ** 2) != n):
+                    divisorsList.append(n // i)
+
+        divisorsList.sort()
+        return divisorsList
+
+    def checkPerfectNumber(self, num: int) -> bool:
+
+        divisors = self.getMyDivisor(num)
+        divisors.pop()
+        if sum(divisors) == num: return True
+        else: return False
