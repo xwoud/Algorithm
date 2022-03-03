943baf7ec8c36be48dd4f54f494401c0d17435e7
mini <xwoud@naver.com>
2022-03-04 00:58:08 +0900

    [upload] 프로그래머스 - 신고 결과 받기 파이썬 제출

---
+++ programmers/신고결과받기.py
@@ -0,0 +1,16 @@
+def solution(id_list, report, k):
+    answer = {}
+    dictionary = {string : 0 for string in id_list}
+    for a in set(report):
+        victim, offender = a.split()[0], a.split()[1]
+        if answer.get(offender) != None : 
+            answer[offender] = answer[offender] | {victim}
+        else:
+            answer[offender] = {victim}
+
+    for counter in answer.values():
+        if len(counter) >= k :
+            for t in counter:
+                dictionary[t] = dictionary[t] + 1
+    
+    return list(dictionary.values())
